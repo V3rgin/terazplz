74f31cb1da65ddd30e562494aa34e0d319414341
Vergin <mrtemnmie@gmail.com>
2024-11-05 14:24:07 +0100

    tragedia2

--- centrala/assets/js/main.js
+++ centrala/assets/js/main.js
@@ -6,6 +6,24 @@ $(document).ready(function () {
 function createMenu() {
 	$.getJSON('menu.json', function(json) {
 		var menu = json.menu;
-              // tutaj kod
-	});
+        htmlMenu = '';
+        for (let i = 0; i < menu.length; i++) {
+            htmlMenu = htmlMenu + '<li><a href="' + menu[i].url + '">'+ menu[i].name + '</a></li>';
+        }
+        $('.sidebar-menu').html(htmlMenu);
+    });
+}
+
+function loadView(){
+    $('.sidebar-menu').on('click', 'a', function(e){
+        var htmlFile = $(this).attr('href');
+        $('#main-content').load("views/", htmlFile);
+        helperHideSidebar();
+        e.preventDefault();
+    });
+}
+
+function helperHideSidebar(){
+    $('#overlay-menu').hide();
+    $('$sidebar').animate({left: "-280px"}, 300);
 }
